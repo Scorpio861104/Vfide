7aab0ede12cbd2bd1bdf74c8e60fd4225dfa376d
Scorpio861104 <markebutters@gmail.com>
2026-01-14 19:08:08 +0000

    style: fix focus:z-[9999] to focus:z-9999 in accessibility.tsx

--- lib/accessibility.tsx
+++ lib/accessibility.tsx
@@ -133,7 +133,7 @@ export function SkipToContent() {
   return (
     <a
       href="#main-content"
-      className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[9999] focus:bg-blue-600 focus:text-white focus:px-4 focus:py-2 focus:rounded-lg focus:shadow-lg"
+      className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-9999 focus:bg-blue-600 focus:text-white focus:px-4 focus:py-2 focus:rounded-lg focus:shadow-lg"
     >
       Skip to main content
     </a>
